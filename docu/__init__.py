e6e883ba3a27d045bc7db328f96bfc69da7c7fba
Florian Stadler <florian@pulumi.com>
2025-01-24 14:17:51 +0100

    Changes

--- docu/__init__.py
+++ docu/__init__.py
@@ -33,72 +33,37 @@ def render(yaml_text, lang):
         return code
 
 
-def update_choosable(s):
-    yaml_text = extract_yaml(s.group(0))
-    if not yaml_text:
-        return s.group(0)
-    yaml_text = yaml_text.strip()
-    return render_code({
-        "yaml": yaml_text,
-        "typescript": render(yaml_text, "typescript"),
-        "python": render(yaml_text, "python"),
-        "go": render(yaml_text, "go"),
-        "csharp": render(yaml_text, "csharp"),
-        "java": render(yaml_text, "java"),
-    })
-
-
-def extract_yaml(text):
-    pat = re.compile(r"""```yaml([^`]*)```""", re.DOTALL)
-    m = pat.search(text)
-    if m:
-        code = m.group(1)
-        return code
-
-
-def replace_choosables(s):
-    pat = re.compile(r"""{{< chooser[^>]+>}}(?:[^{]|[{](?!{<))*{{< /chooser >}}""", re.DOTALL)
-    return re.sub(pat, update_choosable, s)
-
-
 def render_choosable(lang, source):
     return f"""
-{{{{% choosable language {lang} %}}}}
-
 ```{lang}
 {source}
 ```
-
-{{{{% /choosable %}}}}
 """
 
 
-def render_code(sources):
-    selected_langs = []
-    for lang in ["typescript", "python", "go", "csharp", "java", "yaml"]:
-        if lang in sources:
-            selected_langs.append(lang)
-    body = "".join([render_choosable(l, sources[l]) for l in selected_langs])
-    languages = ",".join(selected_langs)
-    return f"""{{{{< chooser language "{languages}" >}}}}
-{body}
-{{{{< /chooser >}}}}"""
+def render_code(yaml_text):
+    sources = {
+        "yaml": yaml_text,
+    }
+    for lang in ["typescript", "python", "go", "csharp"]:
+        sources[lang] = render(yaml_text, lang)
+    return "".join([render_choosable(l, sources[l]) for l in sources.keys()])
 
 
 def main_cli():
     argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument("doc", help="markdown file")
+    argparser.add_argument("yaml", help="Pulumi YAML file")
     argparser.add_argument("-o", help="output file")
     args = argparser.parse_args()
 
-    doc = args.doc
+    yaml = args.yaml
     o = args.o
 
-    with open(doc, "r") as f:
-        doc_text = f.read()
+    with open(yaml, "r") as f:
+        yaml_text = f.read()
 
     with open(o, "w") as fp:
-        fp.write(replace_choosables(doc_text))
+        fp.write(render_code(yaml_text))
 
 
 if __name__ == "__main__":
